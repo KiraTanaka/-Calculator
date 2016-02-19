bdb019cb2936041ac2eab101ccced0444401717e
Kira Tanaka <suein_i@mail.ru>
2016-02-19 22:30:11 +0500

    Adding test for subtraction

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -7,18 +7,24 @@ namespace CalculatorTests
     [TestFixture]
     public class CalculatorTests
     {
+        private float firstNumber = 3;
+        private float secondNumber = 4;
 
         [Test]
         public void Addition()
         {
-            float firstNumber = 3;
-            float secondNumber = 4;
-
             float resultOfAddition = Calculator.Addition(firstNumber,secondNumber);
 
             Assert.AreEqual(7,resultOfAddition);
         }
 
+        [Test]
+        public void Subtraction()
+        {
+            float resultOfSubtraction = Calculator.Subtraction(firstNumber, secondNumber);
+
+            Assert.AreEqual(1, resultOfAddition);
+        }
 
     }
 }
