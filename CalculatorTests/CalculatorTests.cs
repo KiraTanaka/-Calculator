9c898034d832404b66e0bd4d43f02763260c653f
Kira Tanaka <suein_i@mail.ru>
2016-02-19 21:30:34 +0500

    Adding test for addition

---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+
+        [Test]
+        public void AdditionTest()
+        {
+            var calculator = new Calculator();
+            float firstNumber = 3;
+            float secondNumber = 4;
+
+            int resultOfAddition = calculator.Addition(firstNumber,secondNumber);
+
+            Assert.AreEqual(7,resultOfAddition);
+        }
+
+    }
+}
