249af915baab272adf0db3598cca3bf15c9c8417
Kira Tanaka <suein_i@mail.ru>
2016-02-19 22:55:30 +0500

    Adding test for division

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -7,15 +7,15 @@ namespace CalculatorTests
     [TestFixture]
     public class CalculatorTests
     {
-        private float firstNumber = 3;
-        private float secondNumber = 4;
+        private float firstNumber = 3.5f;
+        private float secondNumber = 7f;
 
         [Test]
         public void Addition()
         {
             float resultOfAddition = Calculator.Addition(firstNumber,secondNumber);
 
-            Assert.AreEqual(7,resultOfAddition);
+            Assert.AreEqual(9.5f,resultOfAddition);
         }
 
         [Test]
@@ -23,7 +23,7 @@ namespace CalculatorTests
         {
             float resultOfSubtraction = Calculator.Subtraction(firstNumber, secondNumber);
 
-            Assert.AreEqual(1, resultOfSubtraction);
+            Assert.AreEqual(3.5f, resultOfSubtraction);
         }
 
         [Test]
@@ -31,8 +31,14 @@ namespace CalculatorTests
         {
             float resultOfMultiplication = Calculator.Multiplication(firstNumber, secondNumber);
 
-            Assert.AreEqual(12, resultOfMultiplication);
+            Assert.AreEqual(24.5f, resultOfMultiplication);
         }
+        [Test]
+        public void Division()
+        {
+            float resultOfDivision = Calculator.Division(firstNumber, secondNumber);
 
+            Assert.AreEqual(2, resultOfDivision);
+        }
     }
 }
