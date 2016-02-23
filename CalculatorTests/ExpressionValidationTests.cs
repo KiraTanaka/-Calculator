21b1f123b0b17292e202ed19cf65665e78f58d35
Kira Tanaka <suein_i@mail.ru>
2016-02-23 13:23:46 +0500

    Adding test for CheckingSigns

--- CalculatorTests/ExpressionValidationTests.cs
+++ CalculatorTests/ExpressionValidationTests.cs
@@ -9,6 +9,7 @@ namespace ConsoleCalculatorTests
     {
         private string correctExpression;
         private string incorrectExpression;
+
         [Test]
         public void CheckingBracketsWithCorrectExpressionTest()
         {
@@ -22,6 +23,20 @@ namespace ConsoleCalculatorTests
             incorrectExpression = "2*)(3+5)+(7-3))";
 
             Assert.IsFalse(ExpressionValidation.CheckingBrackets(incorrectExpression));
+        }
+        [Test]
+        public void CheckingSignsWithCorrectExpressionTest()
+        {
+            correctExpression = "(2*(3+5))+(7-3)";
+
+            Assert.IsTrue(ExpressionValidation.CheckingSigns(correctExpression));
+        }
+        [Test]
+        public void CheckingSignsWithIncorrectExpressionTest()
+        {
+            correctExpression = "+((-2)*(3+(-5)/)*9)+(7-*3)";
+
+            Assert.IsFalse(ExpressionValidation.CheckingSigns(correctExpression));
         }
     }
 }
