58b9db42a61902a948264d872d1aae8821b7d2ae
Kira Tanaka <suein_i@mail.ru>
2016-02-24 21:48:57 +0500

    Adding test for Parsing

---
+++ CalculatorTests/ParserExpressionTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using ConsoleCalculator;
+
+namespace ConsoleCalculatorTests
+{
+    [TestFixture]
+    public class ParserExpressionTests
+    {
+        [Test]
+        public void ParsingTest()
+        {
+            string expression = "((-2)*(3+5))+(7-3)";
+
+            Assert.AreEqual(-12,ParserExpression.Parsing(expression));
+        }
+    }
+}
