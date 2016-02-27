9b9960a51d789c1c91cf256bfe221a81320f2f28
Kira Tanaka <suein_i@mail.ru>
2016-02-27 17:35:36 +0500

    Adding test for SplitExpressionIntoElements

--- CalculatorTests/EvaluationOfExpressionTests.cs
+++ CalculatorTests/EvaluationOfExpressionTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NUnit.Framework;
 using ConsoleCalculator;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ConsoleCalculatorTests
 {
@@ -13,6 +15,38 @@ namespace ConsoleCalculatorTests
             string expression = "((-2)*(3+5))+(7-3)";
 
             Assert.AreEqual(-12, EvaluationOfExpression.Evaluation(expression));
+        }
+        [Test]
+        public void SplitExpressionIntoElementsTest()
+        {
+            string expression = "(-2)*(3+5.8)+(7-3)";
+            Stack<string> elementsOfExpression = new Stack<string>();
+            Stack<string> result = new Stack<string>();
+            elementsOfExpression.Push(")");
+            elementsOfExpression.Push("3");
+            elementsOfExpression.Push("-");
+            elementsOfExpression.Push("7");
+            elementsOfExpression.Push("(");
+            elementsOfExpression.Push("+");
+            elementsOfExpression.Push(")");
+            elementsOfExpression.Push("5.8");
+            elementsOfExpression.Push("+");
+            elementsOfExpression.Push("3");
+            elementsOfExpression.Push("(");
+            elementsOfExpression.Push("*");
+            elementsOfExpression.Push(")");
+            elementsOfExpression.Push("-2");
+            elementsOfExpression.Push("(");
+           
+            result = EvaluationOfExpression.SplitExpressionIntoElements(expression);
+            int count = elementsOfExpression.Count;
+
+            Assert.AreEqual(elementsOfExpression.Count, result.Count);
+
+            for (int i = 0; i < count; i++)
+            {
+                Assert.AreEqual(elementsOfExpression.Pop(), result.Pop());
+            }
         }
     }
 }
